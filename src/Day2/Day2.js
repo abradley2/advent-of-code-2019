05cfc6ace76e451b92ae849b8f8edfeca4bdafb1
Anthony Bradley <antbradley91@gmail.com>
2019-12-03 09:17:26 -0500

    remove part 0

--- src/Day2/Day2.js
+++ src/Day2/Day2.js
@@ -4,6 +4,5 @@ import PartOne from "./PartOne.txt";
 
 import { mountSolution } from "../advent-utils";
 
-mountSolution(Elm.Day2, 0, PartZero);
 mountSolution(Elm.Day2, 1, PartOne);
 mountSolution(Elm.Day2, 2, PartOne)
