999c232257c07a03cc1917dc15fb7850842fa149
Anthony Bradley <antbradley91@gmail.com>
2019-12-17 12:40:50 -0500

    day 6 part 2

--- src/Day6/Day6.elm
+++ src/Day6/Day6.elm
@@ -1,5 +1,7 @@
 module Day6 exposing (main)
 
+import Array
+import Basics.Extra exposing (flip)
 import Browser exposing (element)
 import Html as H
 import List.Extra as ListX
@@ -13,6 +15,20 @@ type alias Orbit =
     { parent : String
     , child : String
     }
+
+
+sliceFromIndex : Int -> List a -> List a
+sliceFromIndex idx list =
+    ListX.indexedFoldl
+        (\idx_ item all ->
+            if idx_ >= idx then
+                all ++ [ item ]
+
+            else
+                all
+        )
+        []
+        list
 
 
 getChildren : List Orbit -> String -> List String
@@ -78,16 +94,20 @@ type Node
     = Planet String (List Node)
 
 
-getNumberOfOrbits : String -> Int -> Node -> Maybe Int
-getNumberOfOrbits target curDistance node =
+getNumberOfOrbits : String -> Int -> List String -> Node -> Maybe ( Int, List String )
+getNumberOfOrbits target curDistance path node =
     case node of
         Planet nameTag children ->
+            let
+                currentPath =
+                    path ++ [ nameTag ]
+            in
             if nameTag == target then
-                Just curDistance
+                Just ( curDistance, currentPath )
 
             else
                 List.map
-                    (getNumberOfOrbits target (curDistance + 1))
+                    (getNumberOfOrbits target (curDistance + 1) currentPath)
                     children
                     |> List.filter MaybeX.isJust
                     |> List.head
@@ -120,6 +140,69 @@ type alias Solution =
     Result String String
 
 
+closestIntersect : List String -> List String -> Maybe String
+closestIntersect pathA pathB =
+    let
+        pathBSet =
+            Set.fromList pathB
+    in
+    List.foldl
+        (\pathAPlanet found ->
+            if Set.member pathAPlanet pathBSet then
+                Just pathAPlanet
+
+            else
+                found
+        )
+        Nothing
+        pathA
+
+
+partTwo : String -> Solution
+partTwo input =
+    inputToOrbitDict input
+        |> Result.andThen
+            (\( allOrbits, sun ) ->
+                let
+                    solarSystem =
+                        buildSolarSystem allOrbits "COM"
+
+                    pathMeResult =
+                        getNumberOfOrbits "YOU" 0 [] solarSystem
+
+                    pathSantaResult =
+                        getNumberOfOrbits "SAN" 0 [] solarSystem
+
+                    lastIntersect =
+                        Maybe.map2
+                            (\a b -> ( closestIntersect a b, a, b ))
+                            (Maybe.map Tuple.second pathMeResult)
+                            (Maybe.map Tuple.second pathSantaResult)
+                            |> Maybe.andThen
+                                (\( mIntersect, idxA, idxB ) ->
+                                    Maybe.map
+                                        (\intersect -> ( intersect, idxA, idxB ))
+                                        mIntersect
+                                )
+                            |> Maybe.andThen
+                                (\( intersect, pathA, pathB ) ->
+                                    Maybe.map2
+                                        (\idxA idxB ->
+                                            sliceFromIndex idxA pathA ++ sliceFromIndex idxB pathB
+                                        )
+                                        (ListX.elemIndex intersect pathA)
+                                        (ListX.elemIndex intersect pathB)
+                                )
+                            -- remove duplicate
+                            |> Maybe.map ListX.unique
+                            -- don't count the initial orbits
+                            |> Maybe.map (List.length >> flip (-) 3 >> String.fromInt)
+                            |> Result.fromMaybe "Could not find distance"
+                in
+                lastIntersect
+            )
+
+
 partOne : String -> Solution
 partOne input =
     let
@@ -138,11 +221,11 @@ partOne input =
                             (\planetTag totalDist ->
                                 let
                                     distResult =
-                                        getNumberOfOrbits planetTag 0 solarSystem
+                                        getNumberOfOrbits planetTag 0 [] solarSystem
                                             |> Result.fromMaybe "distance not found"
                                 in
                                 Result.map
-                                    ((+) totalDist)
+                                    (Tuple.first >> (+) totalDist)
                                     distResult
                                     |> Result.withDefault 1000000000000
                             )
@@ -158,6 +241,9 @@ solve problem =
     case problem.part of
         1 ->
             partOne problem.input
+
+        2 ->
+            partTwo problem.input
 
         _ ->
             Result.Err
--- src/Day6/Day6.js
+++ src/Day6/Day6.js
@@ -1,5 +1,7 @@
 import { Elm } from "./Day6.elm";
 import PartOne from "./PartOne.txt";
+import PartTwo from "./PartTwo.txt";
 import { mountSolution } from "../advent-utils";
 
 mountSolution(Elm.Day6, 1, PartOne);
+mountSolution(Elm.Day6, 2, PartTwo)
